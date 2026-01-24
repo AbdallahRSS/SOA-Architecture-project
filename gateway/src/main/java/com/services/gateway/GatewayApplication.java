6a12d1413b8c015280f059aab40c3f78b872f8c4
AbdallahiRSS <23030@supnum.mr>
2026-01-24 14:31:05 +0000

    add api-gateway service

---
+++ gateway/src/main/java/com/services/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.services.gateway;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer   
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
